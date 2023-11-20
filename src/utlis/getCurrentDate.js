a718a8ab985f75ce3cc3b68fb67e97bf867c5057
zordan <zekagorobey@gmail.com>
2023-11-20 17:04:32 +0200

    change date format

--- src/utlis/getCurrentDate.js
+++ src/utlis/getCurrentDate.js
@@ -1,13 +1,15 @@
 export const getCurrentDate = () => {
   const currentDate = new Date();
-  const monthNumber = (currentDate.getMonth()).toString();
-  const day = currentDate.getDate().toString();
+  const monthNumber = (currentDate.getMonth()).toString().padStart(2, '0');
+  const day = currentDate.getDate().toString().padStart(2, '0');
+  const hours = currentDate.getHours().toString().padStart(2, '0');
+  const minutes = currentDate.getMinutes().toString().padStart(2, '0');
   const months = [
     'January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December'
   ];
 
-  const formattedDate = `${months[monthNumber]}-${day}`;
+  const formattedDate = `${months[monthNumber]}-${day}, ${hours}:${minutes}`;
 
   return formattedDate;
 }
